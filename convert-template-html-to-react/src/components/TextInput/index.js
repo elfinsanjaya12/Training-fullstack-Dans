a4095be30dab9e15967a72b6945acf64d58e40c5
elfinsanjaya12 <elfinsanjaya1207@gmail.com>
2022-09-20 16:36:26 +0700

    feat: component input with label

---
+++ convert-template-html-to-react/src/components/TextInput/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Form } from 'react-bootstrap';
+
+export default function TextInput({
+  name,
+  value,
+  type,
+  onChange,
+  placeholder,
+  disabled,
+}) {
+  return (
+    <Form.Control
+      type={type}
+      name={name}
+      value={value}
+      placeholder={placeholder}
+      onChange={onChange}
+      disabled={disabled}
+    />
+  );
+}
---
+++ convert-template-html-to-react/src/components/TextInputWithLabel/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Form } from 'react-bootstrap';
+import TextInput from '../TextInput';
+
+export default function TextInputWithLabel({
+  label,
+  name,
+  value,
+  type,
+  onChange,
+  disabled,
+  placeholder,
+}) {
+  return (
+    <Form.Group className='mb-2'>
+      <Form.Label>{label}</Form.Label>
+      <TextInput
+        type={type}
+        name={name}
+        value={value}
+        placeholder={placeholder}
+        onChange={onChange}
+        disabled={disabled}
+      />
+    </Form.Group>
+  );
+}
